ee17d9978d6b428ce50e6512202c50758aaa03fb
ysmarco <ysmarco12@naver.com>
2024-10-09 17:55:23 +0900

    오탈자

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
 </template>
 <script>
 
-import MainVisual from '@/components/MainVIsual.vue';
+import MainVisual from '@/components/MainVisual.vue';
 import ProjectScroll from '@/components/ProjectScroll.vue';
 import EtcProject from '@/components/EtcProject.vue';
 import BottomProfile from '@/components/BottomProfile.vue';
